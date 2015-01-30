1ad2e3b38adc5d1beb0d00ff2d679c10957dc9ad
kjmonaghan <kmonaghan247@gmail.com>
2015-01-30 17:02:12 -0600

    Small clean-up
    Removed unnecessary copies of the sprites and removed a couple unused
    imports.

--- src/GUI/ChessWindow.java
+++ src/GUI/ChessWindow.java
@@ -23,8 +23,6 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Point;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -32,9 +30,6 @@ import java.awt.event.MouseMotionListener;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 import Game.Board;
